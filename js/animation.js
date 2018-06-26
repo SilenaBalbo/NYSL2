3acae091b7ca6ea418f280db79c72925b30bf2e3
SilenaBalbo <silenabalbo@hotmail.com>
2018-06-26 14:49:28 +0200

    first commit

---
+++ js/animation.js
@@ -0,0 +1,19 @@
+/*eslint-env browser*/
+/*eslint "no-console": "off" */
+
+/* page loader */
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
